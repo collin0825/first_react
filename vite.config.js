5c2eec574e43ee207711e605e6047fa478bd431a
collin <collin0825@gmail.com>
2023-07-26 22:31:51 +0800

    edit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 產品路徑、開發中
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/first_react/' : '/',
   plugins: [react()],
 })
